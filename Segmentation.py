44391feb50e798501e04a4176812f29a71c0a1ba
RaphaelNadeau <58865095+RaphaelNadeau@users.noreply.github.com>
2021-07-27 15:15:17 -0400

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -10,6 +10,18 @@ src = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_3+4.tif"
 image = imread(src)
 print(image.dtype)
 
-image = cv2.normalize(image, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
-print(image.dtype)
+image = np.float32(image)
+print(image.dtype, image.shape)
+img = image.reshape(-1)
+
+criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
+K = 10
+ret, label, center = cv2.kmeans(img, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+
+center = np.uint8(center)
+res = center[label.flatten()]
+res2 = res.reshape(image.shape)
+cv2.imshow("res2", res2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
 

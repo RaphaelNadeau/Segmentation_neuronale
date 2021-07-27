cbd3f8f64e42318fe724277e41185f44f92d0a43
RaphaelNadeau <58865095+RaphaelNadeau@users.noreply.github.com>
2021-07-27 14:30:25 -0400

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -1,2 +1,15 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from tifffile import imread, imsave, imshow
+import matplotlib.pyplot as plt
+import time
+import cv2
+
+src = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_3+4.tif"
+
+image = imread(src)
+print(image.dtype)
+
+image = cv2.normalize(image, dst=None, alpha=0, beta=255, norm_type=cv2.NORM_MINMAX, dtype=cv2.CV_8U)
+print(image.dtype)
+

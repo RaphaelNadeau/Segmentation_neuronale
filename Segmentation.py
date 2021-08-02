ac10a5906d96f147ea871d6a9f464c652bf85aaa
RaphaelNadeau <58865095+RaphaelNadeau@users.noreply.github.com>
2021-08-02 16:09:23 -0400

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -1,27 +1,63 @@
-import matplotlib.pyplot as plt
 import numpy as np
-from tifffile import imread, imsave, imshow
 import matplotlib.pyplot as plt
-import time
-import cv2
+from sklearn.cluster import KMeans
+from sklearn.utils import shuffle
+from skimage import img_as_ubyte
+from time import time
+import cv2 as cv
 
-src = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_3+4.tif"
+n = 7
+src = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_3+4.jpg"
+img = cv.imread(src)
+# image = cv.imread(src)
+print(img.dtype)
+image = np.array(img, dtype=np.float64) / 255
+print(image.shape)
 
-image = imread(src)
-print(image.dtype)
+w, h, d = original_shape = tuple(image.shape)
+assert d == 3
+image_array = np.reshape(image, (w * h, d))
 
-image = np.float32(image)
-print(image.dtype, image.shape)
-img = image.reshape(-1)
+print("Fitting model on a small sub-sample of the data")
+t0 = time()
+image_array_sample = shuffle(image_array, random_state=0)[:1000]
+kmeans = KMeans(n_clusters=n, random_state=0).fit(image_array_sample)
+print("done in %0.3fs." % (time() - t0))
 
-criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 10
-ret, label, center = cv2.kmeans(img, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
+print("Predicting color indices on the full image (k-means)")
+t0 = time()
+labels = kmeans.predict(image_array)
+# print(kmeans.cluster_centers_)
 
-center = np.uint8(center)
-res = center[label.flatten()]
-res2 = res.reshape(image.shape)
-cv2.imshow("res2", res2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print("done in %0.3fs." % (time() - t0))
 
+
+def recreate_image(codebook, labels, w, h):
+    """Recreate the (compressed) image from the code book & labels"""
+    d = codebook.shape[1]
+    image = np.zeros((w, h, d))
+    label_idx = 0
+    for i in range(w):
+        for j in range(h):
+            image[i][j] = codebook[labels[label_idx]]
+            label_idx += 1
+    return image
+
+
+# Display all results, alongside original image
+plt.figure(1)
+plt.clf()
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(img)
+
+plt.figure(2)
+plt.clf()
+plt.axis('off')
+plt.title('Quantized image (64 colors, K-Means)')
+plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+# plt.show()
+
+gray_value_ubyte= img_as_ubyte(kmeans.cluster_centers_)
+print(gray_value_ubyte)
+print(np.max(gray_value_ubyte))

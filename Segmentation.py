54ee77075cd91bb225fc2494b78ce88bc38452ec
RaphaelNadeau <58865095+RaphaelNadeau@users.noreply.github.com>
2021-08-03 16:16:29 -0400

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -6,13 +6,11 @@ from skimage import img_as_ubyte
 from time import time
 import cv2 as cv
 
-n = 7
+n = 10
 src = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_33+34.jpg"
 img = cv.imread(src)
-# image = cv.imread(src)
-print(img.dtype)
 image = np.array(img, dtype=np.float64) / 255
-print(image.shape)
+
 
 w, h, d = original_shape = tuple(image.shape)
 assert d == 3
@@ -27,8 +25,6 @@ print("done in %0.3fs." % (time() - t0))
 print("Predicting color indices on the full image (k-means)")
 t0 = time()
 labels = kmeans.predict(image_array)
-# print(kmeans.cluster_centers_)
-
 print("done in %0.3fs." % (time() - t0))
 
 
@@ -44,40 +40,42 @@ def recreate_image(codebook, labels, w, h):
     return image
 
 
-# # Display all results, alongside original image
+# Display all results, alongside original image
 # plt.figure(1)
 # plt.clf()
-# plt.axis('off')
-# plt.title('Original image (96,615 colors)')
-# plt.imshow(img)
-#
+plt.subplot(2, 1, 1)
+plt.axis('off')
+plt.title('Original image (96,615 colors)')
+plt.imshow(img)
+
 # plt.figure(2)
 # plt.clf()
-# plt.axis('off')
-# plt.title('Quantized image (64 colors, K-Means)')
-# plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
+plt.subplot(2, 1, 2)
+plt.axis('off')
+plt.title('Quantized image (3 colors, K-Means)')
+plt.imshow(recreate_image(kmeans.cluster_centers_, labels, w, h))
 # plt.show()
+plt.clf()
 
-gray_value_ubyte = img_as_ubyte(kmeans.cluster_centers_)
-gray_sorted = np.argsort(gray_value_ubyte, axis=0)
-# print(gray_value_ubyte)
-# print(gray_sorted)
-# print(gray_sorted[-3:, 0])
-# print(np.max(gray_value_ubyte))
-# print(kmeans.labels_)
-# print(labels)
-# print(labels.shape, kmeans.labels_.shape)
-# print(np.argmax(gray_value_ubyte, axis=0))
-# print(gray_sorted[-3:, 0] in kmeans.labels_)
+
 
 # ## Make a binary mask of the image ##
-# binary = np.zeros(524288)
-# for i in range(0, 524288, 1):  # 524288, 1000
-#     if labels[i] in gray_sorted[-3:, 0]:
-#         binary[i] = True
-#     else:
-#         binary[i] = False
-# # print(binary)
-# binary = np.reshape(binary, (512, 1024))
-# plt.imshow(binary)
-# plt.show()
+def binary(codebook, labels):
+    gray_value_ubyte = img_as_ubyte(codebook)
+    gray_sorted = np.argsort(gray_value_ubyte, axis=0)
+    binaire = np.zeros(524288)
+    for i in range(0, 524288, 1):  # 524288, 1000
+        if labels[i] in gray_sorted[-4:, 0]:
+            binaire[i] = True
+        else:
+            binaire[i] = False
+    # print(binary)
+    binaire = np.reshape(binaire, (512, 1024))
+    return binaire
+
+
+binary(kmeans.cluster_centers_, labels)
+plt.axis('off')
+plt.imshow(binaire)
+plt.show()
+

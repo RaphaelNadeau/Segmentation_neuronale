6fea33cfff97e9c6168ad3abf1c6324bff0417e9
RaphaelNadeau <58865095+RaphaelNadeau@users.noreply.github.com>
2021-08-04 15:11:15 -0400

    Update Segmentation.py

--- Segmentation.py
+++ Segmentation.py
@@ -6,38 +6,53 @@ from skimage import img_as_ubyte
 from time import time
 import cv2 as cv
 from scipy import ndimage as nd
+from os import listdir
+from os.path import join
 
 n = 10
-src = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_1+2(contrast).jpg"
-#src1 = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_1+2(contrast).jpg"
+src = r"C:\Users\raper\Desktop\Temp\ratiometrique"
+
+
+# src1 = r"C:\Users\raper\Desktop\Temp\ratiometrique\combined_image_1+2(contrast).jpg"
 
 
 def kmeans_segmentation(source, nb_cluster):
+    """
+    Fonction permettant de ségmenter une image RGB avec un algorithme de KMeans, on obtient alors différentes couleurs
+    moyennes pour les différents segments.
+    :param source: Le fichier image à ségmenter
+    :type source: ndarray
+    :param nb_cluster: Le nombre de division demander pour la segmentation
+    :type nb_cluster: int
+    :return: La valeur moyenne des différents groupes, les étiquettes associées à chaque groupe, la hauteur de l'image
+    et la largeur de l'image
+    :rtype: tuple avec à l'intérieur (ndarray, ndarray, int, int)
+    """
     img = cv.imread(source)
     image = np.array(img, dtype=np.float64) / 255
-    w, h, d = tuple(image.shape)  # La taille originale de l'image
+    h, w, d = tuple(image.shape)  # La taille originale de l'image
     assert d == 3
     image_array = np.reshape(image, (w * h, d))
     image_array_sample = shuffle(image_array, random_state=0)[:1000]
     kmeans = KMeans(init="k-means++", n_clusters=nb_cluster, random_state=0).fit(image_array_sample)
     labels = kmeans.predict(image_array)
-    return kmeans.cluster_centers_, labels, w, h, d
+    return kmeans.cluster_centers_, labels, h, w
 
 
-def recreate_image(codebook, labels, w, h):
+def recreate_image(codebook, labels, height, width):
     """Recreate the (compressed) image from the code book & labels"""
     d = codebook.shape[1]
-    image = np.zeros((w, h, d))
+    image = np.zeros((height, width, d))
     label_idx = 0
-    for i in range(w):
-        for j in range(h):
+    for i in range(height):
+        for j in range(width):
             image[i][j] = codebook[labels[label_idx]]
             label_idx += 1
     return image
 
 
-# ## Make a binary mask of the image ##
-def binary(codebook, labels, width, height, denoising=True):
+## Make a binary mask of the image ##
+def binary(codebook, labels, height, width, denoising=True):
     """
     :param codebook: Les valeurs moyennes des groupes formés par la segmentation par Kmeans
     :type codebook: ndarray
@@ -60,7 +75,7 @@ def binary(codebook, labels, width, height, denoising=True):
             binaire[i] = True
         else:
             binaire[i] = False
-    binaire = np.reshape(binaire, (width, height))
+    binaire = np.reshape(binaire, (height, width))
     if denoising:
         binaire_open = nd.binary_opening(binaire, np.ones((3, 3)))
         binaire_closed = nd.binary_closing(binaire_open, np.ones((3, 3)))
@@ -69,26 +84,23 @@ def binary(codebook, labels, width, height, denoising=True):
         return binaire
 
 
-a, b, c, d, e = kmeans_segmentation(src, n)
-# f, g, h, i, j = kmeans_segmentation(src1, n)
-bin1 = binary(a, b, c, d, denoising=True)
-# bin2 = binary(f, g, h, i, denoising=True)
-bin3 = binary(a, b, c, d, denoising=False)
-# bin4 = binary(f, g, h, i, denoising=False)
-
-plt.subplot(2, 2, 1)
-plt.axis("off")
-plt.imshow(bin1)
-
-plt.subplot(2, 2, 2)
-plt.axis("off")
-# plt.imshow(bin2, cmap="inferno")
-
-plt.subplot(2, 2, 3)
-plt.axis("off")
-plt.imshow(bin3)
-
-plt.subplot(2, 2, 4)
-plt.axis("off")
-# plt.imshow(bin4, cmap="inferno")
-plt.show()
+list_of_files = listdir(src)
+for file in list_of_files:
+    if file.endswith(".png"):
+        # Pour s'assurer de ne pas reségmenter les images qui ont déjà été traitées
+        if "_segmented" in file or "_binary_mask" in file:
+            continue
+        else:
+            filename = join(src, file)  # Le nom complet du fichier en cours
+            file_split = file.split(".")
+            kmean = kmeans_segmentation(filename, n)  # Ségmentation de l'image en cours
+            segmented_image = recreate_image(kmean[0], kmean[1], kmean[2], kmean[3])  # Recréation de l'image ségmentée
+            plt.imsave(src + "\\" + file_split[0] + "_segmented.png", segmented_image)
+            binaire = binary(kmean[0], kmean[1], kmean[2], kmean[3])  # Création d'un masque binaire associé à l'image
+            plt.imsave(src + "\\" + file_split[0] + "_binary_mask.png", binaire, cmap='gray')
+            ## Décomenter si l'on veut voir le mask binaire pendant le traitement des images
+            # plt.imshow(binaire, cmap='gray')
+            # plt.axis("off")
+            # plt.show()
+    else:
+        continue
